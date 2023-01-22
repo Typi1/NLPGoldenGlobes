99dc81bf5ae8bbddaf85f5a1c10772bde727f8de
Ethan Kim <57647850+Typi1@users.noreply.github.com>
2023-01-22 16:51:13 -0600

    Add files via upload

--- extractor.py
+++ extractor.py
@@ -33,9 +33,12 @@ def get_time(tw):
 class Role(Enum):
     LEADING = 0
     ACTOR = 1
-    DIRECTOR = 2
-    EDITOR = 3
-    CINEMATOGRAPHER = 4
+    ACTRESS = 2
+    DIRECTOR = 3
+    EDITOR = 4
+    CINEMATOGRAPHER = 5
+    SCREENWRITER = 6
+    MUSICIAN = 7
 
 # class of possible genres a content can be or an award can be on
 class Genre(Enum):
@@ -45,6 +48,14 @@ class Genre(Enum):
     ACTION = 3
     HORROR = 4
     ANIMATION = 5
+    MUSICAL = 6
+
+# class to distinguish between the different types of winners
+class WinType(Enum):
+    PICTURE = 0
+    SERIES = 1
+    PERSON = 2
+    OTHER = 3
 
 # class representing people   
 class Person:
@@ -65,33 +76,40 @@ class Person:
 
 # class representing content, like movies or tv shows
 class Content:
-    def __init__(self, name: str, genre: Optional[Genre] = None, people: Optional[set[int]] = {}):
+    def __init__(self, name: str, nicknames: Optional[set[str]] = {}, genre: Optional[Genre] = None, people: Optional[set[int]] = {}, contentType: Optional[WinType] = None):
         # content title
         self.name = name
+        # alternate title names
+        self.nicknames = nicknames
         # content genre or None
         self.genre = genre
         # set of ids of people who have a role in the content or empty set
         self.people = people
+        # type of content (shouldn't ever be 2/person)
+        self.contentType = contentType
         
     def __str__(self):
         return self.name
 
 # class representing award
 class Award:
-    def __init__(self, name: str, presenter: Optional[int] = None, nominees: Optional[set[int]] = {}, winner: Optional[int] = None):
+    def __init__(self, name: str, presenter: Optional[int] = None, nomineeType: Optional[WinType] = None, nominees: Optional[set[int]] = {}, winner: Optional[int] = None):
         # award name
         self.name = name
         # id of person who is the presenter or None
         self.presenter = presenter
+        # type of all nominees (picture/series/person)
+        self.nomineeType = nomineeType
         # set of ids of people who are the nominees or empty set
         self.nominees = nominees
         # id of person who is the winner or None
         self.winner = winner
+        
 
     def __str__(self):
         return self.name
 
-# class representing award cerimony event
+# class representing award ceremony event
 class Event:
     def __init__(self, name: str, host: Optional[int] = None, awards: Optional[dict] = {}, contents: Optional[dict] = {}, people: Optional[dict] = {}, name_to_id: Optional[dict] = {}):
         # event name
@@ -102,15 +120,19 @@ class Event:
         self.awards = awards
         # dict of content name to content instance or empty dict
         self.contents = contents
-        # dict of person id to person instance or empty dict
+        # dict of PERSON ID to person instance or empty dict
         self.people = people
-        # mapping between person name and person id or empty dict
+        # mapping between person NAME and PERSON ID or empty dict
         self.name_to_id = name_to_id
 
         #id counter
         self.id_counter = 0
 
     def add_person(self):
+        return
+
+    # adds a person instance along with a set of nicknames. ONLY use for inputting data from parameters.json
+    def add_person_init():
         return
 
     def add_content():
@@ -148,7 +170,17 @@ def find_event_host():
     return
 
 # gets the information from parameters.json and formats it into our knowledge representation scheme
-def input_parameters():
+def input_parameters(params):
+    print(len(params["awards"]))
+    # make an event instance to return
+    for i in range(len(params["awards"])):
+        print(params["awards"][i]["name"])
+        print(params["awards"][i]["nominee-type"])
+        print(params["awards"][i]["nominee-genre"])
+        # create person or movie object here depending on nominee-type
+        # to be used in adding the nominee canonical name and nicknames
+        print(list(params["awards"][i]["nominees"].keys()))
+
     return
 
 # compares knowledge representation scheme with ground truth and outputs what is wrong and what is missing 
@@ -156,12 +188,12 @@ def test():
     return
 
 def main():
-    parameters = load_json('parameters.json')
-    data = load_json(parameters['dataset_name'])
+    parameters = load_json(os.path.dirname(os.path.realpath(__file__)) + r"\parameters.json")
+    #data = load_json(os.path.dirname(os.path.realpath(__file__)) + "\\" + parameters['dataset_name'])
 
-    print(get_user(data[0]))
+    #print(get_user(data[0]))
     
-
+    input_parameters(parameters)
     return
 
 if __name__ == "__main__":
