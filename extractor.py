7bec63e47192e18539f4ec838be80b314e9a86aa
Ethan Kim <57647850+Typi1@users.noreply.github.com>
2023-01-25 20:41:30 -0600

    fixed bug where all events are same

--- extractor.py
+++ extractor.py
@@ -1,7 +1,8 @@
 import json
 import os
 from enum import Enum
-from typing import Optional, TypedDict
+from typing import Optional, TypedDict, Union
+import re as reg
 
 # HELPER FUNCTIONS FOR LOADING/FORMATING THE DATA
 
@@ -93,18 +94,23 @@ class Content:
 
 # class representing award
 class Award:
-    def __init__(self, name: str, nicknames: Optional[set[str]] = {}, presenter: Optional[int] = None, nomineeType: Optional[WinType] = None, nominees: Optional[set[int]] = {}, winner: Optional[int] = None):
+    def __init__(self, name: str, 
+    nicknames: Optional[set[str]] = {}, 
+    presenters: Optional[set[int]] = None, 
+    nomineeType: Optional[WinType] = None, 
+    nominees: Optional[Union[set[int], set[str]]] = {}, 
+    winner: Optional[Union[int, str]] = None):
         # award name
         self.name = name
         # alternate names for the award or empty set
         self.nicknames = nicknames
-        # id of person who is the presenter or None
-        self.presenter = presenter
+        # ids of people who are the presenters or None
+        self.presenters = presenters
         # type of all nominees (picture/series/person) or None
         self.nomineeType = nomineeType
-        # set of ids of people who are the nominees or empty set
+        # set of ids of people or content names (keys) who are the nominees or empty set
         self.nominees = nominees
-        # id of person who is the winner or None
+        # id of person or name of content (key) who is the winner or None
         self.winner = winner
         
 
@@ -112,39 +118,136 @@ class Award:
         return self.name
 
 # class representing award ceremony event
-class Event:
-    def __init__(self, name: str, host: Optional[int] = None, awards: Optional[dict] = {}, contents: Optional[dict] = {}, people: Optional[dict] = {}, name_to_id: Optional[dict] = {}):
+class Event():
+    def __init__(self, name: Optional[str] = None, hosts: Optional[dict] = {}, awards: Optional[dict] = {}, contents: Optional[dict] = {}, people: Optional[dict] = {}, name_to_id: Optional[dict] = {}):
         # event name
         self.name = name
-        # id of person who is the host or None
-        self.host = host
+        # dict of ids of people who are the host to the corresp. person instance or None
+        self.hosts = {}
+        if(hosts != {}):
+            self.hosts = hosts
         # dict of award name to award instance or empty dict
-        self.awards = awards
+        self.awards = {}
+        if(awards != {}):
+            self.awards = awards
         # dict of content name to content instance or empty dict
-        self.contents = contents
+        self.contents = {}
+        if(contents != {}):
+            self.contents = contents
         # dict of PERSON ID to person instance or empty dict
-        self.people = people
+        self.people = {}
+        if(people != {}):
+            self.people = people
         # mapping between person NAME and PERSON ID or empty dict
-        self.name_to_id = name_to_id
+        self.name_to_id = {}
+        if(name_to_id != {}):
+            self.name_to_id = name_to_id
 
         #id counter
         self.id_counter = 0
 
+
+    # only use for adding people from given answers data, not for when doing other stuff later
+    # returns id used by person created
+    def add_person_init(self, psn: str, is_hst: bool):
+        # we should probably improve the immediately proceeding check since name_to_id is intended to be able to create separate ids for diff people with similar names
+        # if the name given is new, create a new id and increment ID counter, then create a person instance
+        if(not (psn in self.name_to_id)):
+            self.name_to_id[psn] = self.id_counter
+            self.people[self.id_counter] = Person(self.id_counter, psn, None, None, None)
+            self.id_counter += 1
+
+        # if the person is a host and they are not already in the host dictionary, add them
+        if(is_hst and not (psn in self.hosts)):
+            self.hosts[self.name_to_id[psn]] = self.people[self.name_to_id[psn]]
+
+        return
+
+    # only use for adding content from given answers data, not for when doing other stuff later
+    def add_content_init(self, con: str, typ: Optional[WinType] = None):
+        if(not (con in self.contents)):
+            self.contents[con] = Content(con, None, None, None, typ)
+
+        return
+
+    # only use for adding awards from given answers data, not for when doing other stuff later
+    # add people/content in the award first!!!
+    def add_award_init(self, awa: str, haswinner: bool, presenters: Optional[set[int]] = None, typ: Optional[WinType] = None, nominees: Optional[Union[set[int], set[str]]] = None, winner: Optional[Union[int, str]] = None):
+        if(not (awa in self.awards)):
+            # convert list of presenter names to a list of corresp. ids
+            if(presenters != None):
+                presenters = list(self.name_to_id[x] for x in presenters)
+
+            if(nominees == None):
+                nominees = {}
+
+            if(typ == WinType.PERSON):
+                if(nominees != None):
+                    nominees = list(self.name_to_id[x] for x in nominees)
+                    
+                    if(winner != None and (not (winner in nominees))):
+                        nominees.append(self.name_to_id[winner])
+                
+                if(haswinner and winner != None):
+                    winner = self.name_to_id[winner]
+                    
+            elif(nominees != {}):
+                nominees = list(x for x in nominees)
+                if(winner != None and (not (winner in nominees))):
+                        nominees.append(winner)
+
+            if(not haswinner):
+                winner = None
+                
+            
+            self.awards[awa] = Award(awa, None, presenters, typ, nominees, winner)
+
+        return
+
     def add_person(self):
         return
 
-    # adds a person instance along with a set of nicknames. ONLY use for inputting data from parameters.json
-    def add_person_init():
-        return
-
-    def add_content():
+    def add_content(self):
         return 
 
-    def add_award():
+    def add_award(self):
         return
 
     def __str__(self):
-        return self.name
+
+        peop = ""
+        for x in self.people:
+            peop += str(self.people[x]) + "\n\t\t"
+
+        cont = ""
+        for x in self.contents:
+            cont += str(self.contents[x].contentType) + ": " + str(self.contents[x]) + "\n\t\t  "
+
+        awar = ""
+        for x in self.awards:
+            wnr = ""
+            tempwin = self.awards[x].winner
+            if(tempwin != None):
+                if(self.awards[x].nomineeType == WinType.PERSON):
+                    wnr = str(self.people[tempwin])
+                else:
+                    wnr = str(tempwin)
+
+            noms = ""
+            for y in self.awards[x].nominees:
+                if(self.awards[x].nomineeType == WinType.PERSON):
+                    noms += str(self.people[y]) + "\n\t\t\t\t  "
+                else:
+                    noms += str(y) + "\n\t\t\t\t  "
+            noms = noms[:-7]
+
+            awar += str(self.awards[x]) + "\n\t\t\tnominees: " + noms + "\n\t\t\twinner: " + wnr + "\n\n\t\t" 
+
+        return  "event_name: " + self.name + "\n\t" + \
+                "hosts: " + str(list(str(self.people[x]) for x in self.hosts)) + "\n\t" + \
+                "people: " + peop + "\n\t" + \
+                "contents: " + cont + "\n\t" + \
+                "awards: " + awar
 
 # FUNCTIONS TO COMPLETE THE TASKS
 """
@@ -173,29 +276,106 @@ def find_event_host():
 
 # gets the information from parameters.json and formats it into our knowledge representation scheme
 def input_parameters(params):
-    print(len(params["awards"]))
-    # make an event instance to return
-    for i in range(len(params["awards"])):
-        print(params["awards"][i]["name"])
-        print(params["awards"][i]["nominee-type"])
-        print(params["awards"][i]["nominee-genre"])
-        # create person or movie object here depending on nominee-type
-        # to be used in adding the nominee canonical name and nicknames
-        print(list(params["awards"][i]["nominees"].keys()))
+    #print(len(params["award_data"]))
+    #print(list(params["award_data"].keys()))
 
-    return
+    # make multiple event instances for different parts of our tests
+    events = {
+        'event_key': Event("Golden Globes 2013"), # has all the data on hosts, awards, presenters, nominees, winners
+        'event_nom': Event("Golden Globes 2013"), # has data on hosts, awards, presenters, nominees
+        'event_pre': Event("Golden Globes 2013"), # has data on hosts, awards, presenters
+        'event_awa': Event("Golden Globes 2013"), # has data on hosts, awards
+        'event_hos': Event("Golden Globes 2013"), # has data on hosts
+        'event_nul': Event("Golden Globes 2013") # no data
+    }
+
+    counter = 1
+    # add host data/person instances
+    for i in list(events.keys())[:-1]:
+        #print(events[i])
+        for h in params["hosts"]:
+            
+            events[i].add_person_init(h, True)
+            #print(events["event_nul"].hosts)
+            
+            
+
+    # counter = 1
+    # for i in list(events.keys()):
+    #     print(events[i])
+        
+        
+
+
+    for i in list(params["award_data"].keys()):
+        # print(i) # award name
+        # print(params["award_data"][i]["nominees"]) # list of award nominees
+        # print(params["award_data"][i]["presenters"]) # list of presenters for the award
+        # print(params["award_data"][i]["winner"]) # winner of award
+        # NOTE: in the gg2013answers.json, the winner isn't included in the list of nominees so we should adopt this convention for our results file
+
+        wt = None
+        # first determine whether the award is given to content or a person (find the WinType of the nominees and winner)
+        if( reg.search("director", i) != None
+            or reg.search("actor", i) != None
+            or reg.search("actress", i) != None
+            or reg.search("demille", i) != None): # if any of these searches come back with a result, we know the WINTYPE is person
+            wt = WinType.PERSON
+        elif(reg.search("series", i) != None): # if this search comes back with a result, we know the WINTYPE is series
+            wt = WinType.SERIES
+        else: # otherwise, WINTYPE is picture
+            wt = WinType.PICTURE
+
+        # add presenters as people
+        for j in list(events.keys())[:-3]:
+            for pres in params["award_data"][i]["presenters"]:
+                events[j].add_person_init(pres, False)
+
+        # add nominees/winners as either content or people depending on the value of wt
+        if(wt == WinType.PERSON):
+            for j in list(events.keys())[:-4]:
+                for nom in params["award_data"][i]["nominees"]:
+                    events[j].add_person_init(nom, False)
+                events[j].add_person_init(params["award_data"][i]["winner"], False) # adds the winner so that it is included in the nominees
+        elif(wt == WinType.PICTURE or wt == WinType.SERIES):
+            for j in list(events.keys())[:-4]:
+                for nom in params["award_data"][i]["nominees"]:
+                    events[j].add_content_init(nom, wt)
+                events[j].add_content_init(params["award_data"][i]["winner"], wt)
+
+        events['event_key'].add_award_init(i, True, params["award_data"][i]["presenters"], wt, params["award_data"][i]["nominees"], params["award_data"][i]["winner"])
+        
+        events['event_nom'].add_award_init(i, False, params["award_data"][i]["presenters"], wt, params["award_data"][i]["nominees"], params["award_data"][i]["winner"])
+
+        events['event_pre'].add_award_init(i, False, params["award_data"][i]["presenters"])
+
+        events['event_awa'].add_award_init(i, False)
+
+    return events
 
 # compares knowledge representation scheme with ground truth and outputs what is wrong and what is missing 
 def test():
     return
 
 def main():
-    parameters = load_json(os.path.dirname(os.path.realpath(__file__)) + r"\parameters.json")
+    parameters = load_json(os.path.dirname(os.path.realpath(__file__)) + r"\gg2013answers.json")
     #data = load_json(os.path.dirname(os.path.realpath(__file__)) + "\\" + parameters['dataset_name'])
 
     #print(get_user(data[0]))
     
-    input_parameters(parameters)
+    events = input_parameters(parameters)
+
+    print(events['event_nom'])
+    # GUIDE:
+    # events = {
+    #     'event_key': Event("Golden Globes 2013"), # has all the data on hosts, awards, presenters, nominees, winners
+    #     'event_nom': Event("Golden Globes 2013"), # has data on hosts, awards, presenters, nominees
+    #     'event_pre': Event("Golden Globes 2013"), # has data on hosts, awards, presenters
+    #     'event_awa': Event("Golden Globes 2013"), # has data on hosts, awards
+    #     'event_hos': Event("Golden Globes 2013"), # has data on hosts
+    #     'event_nul': Event("Golden Globes 2013") # no data
+    # }
+
     return
 
 if __name__ == "__main__":
