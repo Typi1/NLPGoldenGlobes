dac553c33601584c752c2978f0bd711d9f82d6d4
Ethan Kim <57647850+Typi1@users.noreply.github.com>
2023-01-28 21:54:39 -0600

    fixed bug where items are both person and content

--- webscrape.py
+++ webscrape.py
@@ -21,9 +21,20 @@ def imdb_type_check(s):
     content_html = content_soup.find("div", {"class": "lister-item mode-advanced"})
 
     s_type = None
+
+    # added this block since les miserables wasn't working correctly since there was a barebones person entry for it for some reason.
+    # this error may apply to other movies and I think(?) this should cover them but idk. - Ethan
+    if person_html and content_html:
+        try:
+            type_txt = person_html.find("p", {"class": "text-muted text-small"}).text
+        except AttributeError:
+            person_html = None
+
     if person_html:
         name_txt = person_html.findAll("a")[1].text
         name_regex = re.compile(r"[A-Z].*")
+
+        print("name: " + str(name_txt))
 
         type_txt = person_html.find("p", {"class": "text-muted text-small"}).text
         type_regex = re.compile(r"[A-Z].*")
@@ -33,7 +44,16 @@ def imdb_type_check(s):
     elif content_html:
         type_txt = content_html.find("span", {"class": "genre"}).text
         name_txt = content_html.findAll("a")[1].text
+
+        print("name: " + str(name_txt))
     
         s_type = (name_txt ,re.findall("[A-Z][a-z]*", type_txt))
     
     return s_type
+
+def main():
+    #print(imdb_type_check("les miserables"))
+    pass
+
+if __name__ == '__main__':
+    main()
