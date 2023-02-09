546d33cd75b90a516921c5c1aefd8808eb226f82
Ethan Kim <57647850+Typi1@users.noreply.github.com>
2023-02-08 23:11:51 -0600

    Add files via upload

---
+++ getNameSentiment.py
@@ -0,0 +1,80 @@
+import json
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import re as reg
+import extractor
+import webscrape
+from typing import Optional
+import statistics
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+# done by Ethan
+
+def sentAnalysis(pandaf, target: str, rtOn: bool = False, seen: Optional[dict] = None):
+    
+    captures = []
+
+    # iterate over tweets
+    for i in range(pandaf.shape[0]):
+        curr_text = pandaf['text'][i]
+        curr_text = curr_text.replace("\"", "")
+        curr_text = curr_text.replace("“", " u201c ")
+        curr_text = curr_text.replace("”", " u201c ")
+        # curr_text = curr_text.replace("-", "")
+        curr_text = curr_text.replace("#", "")
+        curr_text = curr_text.replace("’", " u201c ")
+        curr_text = curr_text.replace("‘", " u201c ")
+        curr_text = curr_text.replace("@", "")
+        
+        if not rtOn:
+            rt = reg.search("^(?:[rR][tT])\s+", curr_text) # reg.search("\s+(?:rt)\s+", curr_text)
+            if rt == None:
+                rt = reg.search("\s+(?:rt)\s+", curr_text)
+            if rt == None and "u201c" in curr_text:
+                rt = "not None"
+            if rt == None and "http" in curr_text:
+                rt = "not None"
+
+        if not rtOn and rt != None:
+            # if new_seen == None:
+            #     new_seen = {}
+            # new_seen[i] = curr_text
+            continue
+
+        
+
+        if target.lower() in curr_text.lower().replace("best", "").replace("win", "").replace("won",""):
+            captures.append(curr_text) 
+            seen[i] = curr_text
+        
+    votes = []
+
+    ana = SentimentIntensityAnalyzer()
+    for cap in captures:
+        vs = ana.polarity_scores(cap)
+        print(cap + ": " + str(vs['compound']))
+        votes.append(vs['compound'])
+        # print("{:-<65} {}".format(sentence, str(vs)))
+
+    avg = sum(votes) / len(votes)
+    med = statistics.median(votes)
+
+    return (avg, med, seen)
+
+
+
+
+def main():
+    p = os.path.dirname(os.path.realpath(__file__)) + r"\gg2013.json"
+
+    pandaf = pd.read_json(p)
+    (avg, med) = sentAnalysis(pandaf, "sofia vergara", False, {})
+    print("average: " + str(avg))
+    print("median: " + str(med))
+
+
+if __name__ == '__main__':
+    main()
